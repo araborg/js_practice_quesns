382e5ea47acb1e446ab33789462dc1978d812483
araborg <arababs2015@gmail.com>
2026-04-16 22:49:43 +0100

    attempting que: 31

--- quesns_and_ans.js
+++ quesns_and_ans.js
@@ -158,3 +158,9 @@ console.log(gradeScore(85));
 console.log(gradeScore(62));
 console.log(gradeScore(45));
 console.log(gradeScore(30));
+
+/*
+Write a program using a for loop that prints the multiplication table of 5, from 5×1 up to 5×10.
+
+Expected output format:  5 x 1 = 5   /   5 x 2 = 10   ...   5 x 10 = 50
+*/
