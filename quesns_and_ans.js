15d2a2b103fe35c29e0961f8dcd6c441500111b4
araborg <arababs2015@gmail.com>
2026-04-16 21:56:35 +0100

    attempting que: 1

---
+++ quesns_and_ans.js
@@ -0,0 +1,17 @@
+/*
+Declare three variables using const and let: your name (string),
+your age (number), and a boolean called isStudent. Log all 
+three to the console on separate lines.
+
+Que 1:
+*/
+
+const name = "Babatunde";
+const age = 30;
+const isStudent = true;
+
+console.log(name, age, isStudent);
+
+let name2 = "Babatunde";
+let age2 = 30;
+let isStudent2 = true;
